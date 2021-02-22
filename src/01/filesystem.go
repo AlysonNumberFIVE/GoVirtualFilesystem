2e89a4d5ba761c3e54c237a03e25a356419bf97e
AlysonBee <alyson.stars@yahoo.com>
2021-02-22 08:54:12 +0200

    correcting comment syntax from /* to // in filesystem.go

--- src/01/filesystem.go
+++ src/01/filesystem.go
@@ -9,71 +9,46 @@ import (
 // ease of lookup.
 var globalFileTable map[uint64]string
 
-/*
-** The data structure for each file.
-**    name - The name of the file.
-**    pathFromRoot - The absolute path of the file
-**    fileHash - The unique file hash assigned to this file on
-**        creation
-**    fileType - The type of the file.
-**    content - The file's content in bytes.
-**    size - the size in bytes of the file.
-*/
+// The data structure for each file.
 type file struct {
-	name string
-	pathFromRoot string
-	fileHash uint64
-	fileType string
-	content byte
-	size uint64
+	name string // The name of the file.
+	pathFromRoot string // The absolute path of the file.
+	fileHash uint64 // The unique file hash assigned to this file on creation.
+	fileType string // The type of the file.
+	content byte // The file's content in bytes.
+	size uint64 // The size in bytes of the file.
 }
 
-/*
-** The core struct that makes up the filesystem's file/directory
-**      structure.
-**    directory - The name of the current directory we're in.
-**    files - The list of files in this directory.
-**    directories - The list of directories in this directory.
-**    prev - a reference pointer to this directory's parent directory.
-*/
-
+//  The core struct that makes up the filesystem's file/directory
 type fileSystem struct {
-	directory string
-	files []file
-	directories []fileSystem
-	prev *fileSystem
+	directory string // The name of the current directory we're in.
+	files []file // The list of files in this directory.
+	directories []fileSystem // The list of directories in this directory.
+	prev *fileSystem // a reference pointer to this directory's parent directory.
 }
 
 // Root node.
 var root *fileSystem	
 
-/*
-** Scans the current directory and builds the VFS from it.
-*/
+// initFilesystem scans the current directory and builds the VFS from it.
 func initFilesystem() * fileSystem {
 	// recursively grab all files and directories from this level downwards.
 	fmt.Println("Welcome to the tiny virtual filesystem.")
 	return root
 }
 
-/*
-** Resets the VFS and scraps all changes made up to this point.
-**  (basically like a rerun of initFilesystem())
-*/
+// reloadFilesys Resets the VFS and scraps all changes made up to this point.
+// (basically like a rerun of initFilesystem())
 func (root * fileSystem) reloadFilesys() {
 	fmt.Println("Refreshing...")
 }
 
-/*
-** Gracefully ends the current session.
-*/
+// tearDown gracefully ends the current session.
 func (root * fileSystem) tearDown() {
 	fmt.Println("Teardown")
 }
 
-/*
-** Saves the state of the VFS at this time.
-*/
+// saveState aves the state of the VFS at this time.
 func (root * fileSystem) saveState() {
 	fmt.Println("Save the current state of the VFS")
 }
